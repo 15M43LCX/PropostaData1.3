b700cd05f9c4dfa7a07f709e2d0430afc95b706a
15M43LCX <ismaelcarvier@gmail.com>
2026-03-13 14:22:32 -0300

    Remove commented credentials from supabase.ts

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Estas são as suas credenciais que vi nas imagens anteriores
 const supabaseUrl = 'https://zmbhuxvfreityqlimzlg.supabase.co'
 const supabaseKey = 'sb_publishable_9xE2FLbRdpc7od9YuQwitQ_beiDGfhw'
 
