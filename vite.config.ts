72c9d72903bb483ec3c2c240384f87baa4d16b43
15M43LCX <ismaelcarvier@gmail.com>
2026-03-23 11:55:37 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,11 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
-    // AJUSTE AQUI: O caminho agora inclui a pasta 'dist'
-    base: '/', 
-    plugins: [react()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
-    },
-  };
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  resolve: {
+    alias: { '@': path.resolve(__dirname, '.') },
+  },
 });
